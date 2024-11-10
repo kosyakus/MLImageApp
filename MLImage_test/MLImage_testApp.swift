5e65579248e7f81587c0f6015b5875c09f9a862e
Natali <ns@numbuster.com>
2024-11-10 23:04:56 +0300

    Initial Commit

---
+++ MLImage_test/MLImage_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLImage_testApp.swift
+//  MLImage_test
+//
+//  Created by Natalia Sinitsyna on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MLImage_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
